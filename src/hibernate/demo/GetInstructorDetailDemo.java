05fe20a0b3af05397f2bfb67ca1c7777f273d688
Oleksandr <sashko.kondratenko@gmail.com>
2019-02-25 20:19:10 +0100

    Initial commit

---
+++ src/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,45 @@
+package hibernate.demo;
+
+import hibernate.entity.Instructor;
+import hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorDetailDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                                .configure("hibernate.cfg.xml")
+                                .addAnnotatedClass(Instructor.class)
+                                .addAnnotatedClass(InstructorDetail.class)
+                                .buildSessionFactory();
+        // create a session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            //start a transaction
+            session.beginTransaction();
+
+            int id = 29;
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
+            System.out.println("InstructorDetail: " + instructorDetail);
+            System.out.println("The associated Instructor: " + instructorDetail.getInstructor());
+
+
+
+            // commit transaction
+            session.getTransaction().commit();
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
